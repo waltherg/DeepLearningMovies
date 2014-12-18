71b566e09a956e868808354566a3c177e0c56cc1
Georg Walther <georgwalther@gmail.com>
2014-12-18 01:17:18 +0100

    improve memory efficiency

--- KaggleWord2VecUtility.py
+++ KaggleWord2VecUtility.py
@@ -46,10 +46,5 @@ class KaggleWord2VecUtility(object):
         for raw_sentence in raw_sentences:
             # If a sentence is empty, skip it
             if len(raw_sentence) > 0:
-                # Otherwise, call review_to_wordlist to get a list of words
-                sentences.append( KaggleWord2VecUtility.review_to_wordlist( raw_sentence, \
-                  remove_stopwords ))
-        #
-        # Return the list of sentences (each sentence is a list of words,
-        # so this returns a list of lists
-        return sentences
+                yield KaggleWord2VecUtility.review_to_wordlist(raw_sentence,
+                                                               remove_stopwords)
